4c00275c2313ac7b7bdc32b6fcf87b9ecbf347d0
KacperST <kstankiewicz@student.agh.edu.pl>
2022-11-11 20:52:47 +0100

    changed code format

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'pages/question.dart';
+import 'pages/categoryPage.dart';
 
-void main() => runApp(MaterialApp(initialRoute: '/questionPage', routes: {
-'/questionPage': (context) => const Questionpage()
-  }));
-
+void main() => runApp(MaterialApp(initialRoute: '/categoryPage', routes: {
+      '/questionPage': (context) => const Questionpage(),
+      '/categoryPage': (context) => const CategoryPage(),
+}));
