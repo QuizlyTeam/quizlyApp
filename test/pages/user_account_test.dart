f62ae86e7b3816045b5b2969d7323bb6728e94a1
Tosiek <antonizieciak@gmail.com>
2022-11-12 00:32:54 +0100

    Created test for checking if stats are loaded properly.

--- test/pages/user_account_test.dart
+++ test/pages/user_account_test.dart
@@ -11,4 +11,20 @@ void main() {
     await tester.pumpWidget(testWidget);
     expect(find.text('IronMan73'), findsOneWidget);
   });
+
+  testWidgets('Widget UserAccount displays user\'s stats', (WidgetTester tester) async {
+    Widget testWidget = MediaQuery(
+        data: MediaQueryData(),
+        child: MaterialApp(home: UserAccount())
+    );
+    await tester.pumpWidget(testWidget);
+    expect(find.text('Ratio: '), findsOneWidget);
+    expect(find.text('3:1'), findsOneWidget);
+    expect(find.text('Favourite quiz: '), findsOneWidget);
+    expect(find.text('Dogs'), findsOneWidget);
+    expect(find.text('Friends: '), findsOneWidget);
+    expect(find.text('31'), findsOneWidget);
+    expect(find.text('Most points: '), findsOneWidget);
+    expect(find.text('1043'), findsOneWidget);
+  });
 }
