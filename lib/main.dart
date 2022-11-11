40b6c3da5883a98d030e9e170ed7c64904fa56e7
KacperST <kstankiewicz@student.agh.edu.pl>
2022-11-11 12:07:25 +0100

    added routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/question.dart';
-import 'pages/mainPage.dart';
 
 void main() => runApp(MaterialApp(initialRoute: '/questionPage', routes: {
 '/questionPage': (context) => const Questionpage()
