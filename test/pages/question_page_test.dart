68d2b6718b28cb6b2b3ddbbe0c1b674e2b0a0ad6
KacperST <kstankiewicz@student.agh.edu.pl>
2022-11-11 21:07:53 +0100

    added

--- test/pages/question_page_test.dart
+++ test/pages/question_page_test.dart
@@ -4,14 +4,16 @@ import 'package:quizly_app/pages/question.dart';
 
 void main() {
   testWidgets('Widget Home display Image', (WidgetTester tester) async {
-    await tester.pumpWidget(
-        const  Questionpage()
-    );
-    expect(find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
-    expect(find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
-    expect(find.image(const AssetImage('assets/images/settings.png')), findsOneWidget);
-    expect(find.image(const AssetImage('assets/images/flag.png')), findsOneWidget);
-    expect(find.image(const AssetImage('assets/images/time.png')), findsOneWidget);
-
+    await tester.pumpWidget(const Questionpage());
+    expect(
+        find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
+    expect(
+        find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
+    expect(find.image(const AssetImage('assets/images/settings.png')),
+        findsOneWidget);
+    expect(
+        find.image(const AssetImage('assets/images/flag.png')), findsOneWidget);
+    expect(
+        find.image(const AssetImage('assets/images/time.png')), findsOneWidget);
   });
 }
