d3c3fa6467ff02c3348b42048844e13d64acdecd
KacperST <kstankiewicz@student.agh.edu.pl>
2022-11-22 22:54:47 +0100

    fixed question_test

--- test/pages/question_test.dart
+++ test/pages/question_test.dart
@@ -4,20 +4,25 @@ import 'package:quizly_app/pages/question.dart';
 
 void main() {
   testWidgets('Widget Home display Image', (WidgetTester tester) async {
-    await tester.pumpWidget(const Question(
-        question: "Jakiego kraju \n to flaga?",
-        ans1: "Kiribati",
-        ans2: "Liberia",
-        ans3: "Tuvalu",
-        ans4: "Macedonia",
-        correctAnswer: "Kiribati"));
-    expect(
-        find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
-    expect(
-        find.image(const AssetImage('assets/images/back.png')), findsOneWidget);
-    expect(find.image(const AssetImage('assets/images/settings.png')),
-        findsOneWidget);
-    expect(
-        find.image(const AssetImage('assets/images/flag.png')), findsOneWidget);
+    await tester.runAsync(() async {
+      await tester.pumpWidget(const Question(
+          question: "Jakiego kraju \n to flaga?",
+          ans1: "Kiribati",
+          ans2: "Liberia",
+          ans3: "Tuvalu",
+          ans4: "Macedonia",
+          correctAnswer: "Kiribati"),);
+      expect(
+          find.image(const AssetImage('assets/images/back.png')),
+          findsOneWidget);
+      expect(
+          find.image(const AssetImage('assets/images/back.png')),
+          findsOneWidget);
+      expect(find.image(const AssetImage('assets/images/settings.png')),
+          findsOneWidget);
+      expect(
+          find.image(const AssetImage('assets/images/flag.png')),
+          findsOneWidget);
+    });
   });
 }
