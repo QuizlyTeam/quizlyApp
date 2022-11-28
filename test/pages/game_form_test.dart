149daa36ef53501e8b470ffae613c9a85dc678f5
Tosiek <antonizieciak@gmail.com>
2022-11-28 19:07:39 +0100

    Added test for game form.

---
+++ test/pages/game_form_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quizly_app/pages/game_form.dart';
+
+void main() {
+  testWidgets('Widget GameForm properly show information', (WidgetTester tester) async {
+    await tester.pumpWidget(const GameForm());
+
+    expect(find.text('Choose your'), findsOneWidget);
+    expect(find.text('game options:'), findsOneWidget);
+    expect(find.text('Private'), findsNWidgets(2));
+    expect(find.text('Public'), findsOneWidget);
+    expect(find.text('Pick'), findsOneWidget);
+    expect(find.text('Play!'), findsOneWidget);
+  });
+}
