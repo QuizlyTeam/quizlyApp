e657d4b205d9af281ebd16ad3cc225671d46215d
Glonk223 <janek.stop@gmail.com>
2022-12-22 19:41:03 +0100

    fixed category page and tests' format

--- test/pages/category_page_test.dart
+++ test/pages/category_page_test.dart
@@ -19,38 +19,55 @@ void main() {
   });
 
   test("http.get test", () async {
-    final interceptor = nock("http://10.0.2.2:8000/v1/quizzes").get("/categories")
-      ..reply(
-        200,
-        '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}',
-      );
+    final interceptor =
+        nock("http://10.0.2.2:8000/v1/quizzes").get("/categories")
+          ..reply(
+            200,
+            '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}',
+          );
 
-    final response = await http.get(Uri.parse("http://10.0.2.2:8000/v1/quizzes/categories"));
+    final response =
+        await http.get(Uri.parse("http://10.0.2.2:8000/v1/quizzes/categories"));
 
     expect(interceptor.isDone, true);
     expect(response.statusCode, 200);
-    expect(response.body, '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}');
+    expect(response.body,
+        '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}');
   });
 
   testWidgets('Widget display category test', (WidgetTester tester) async {
     await tester.runAsync(() async {
-      final interceptor = nock("http://10.0.2.2:8000").get("/v1/quizzes/categories")
-        ..reply(
-          200,
-          '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}',
-        );
+      final interceptor =
+          nock("http://10.0.2.2:8000").get("/v1/quizzes/categories")
+            ..reply(
+              200,
+              '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}',
+            );
 
-      final response = await http.get(Uri.parse("http://10.0.2.2:8000/v1/quizzes/categories"));
+      final response = await http
+          .get(Uri.parse("http://10.0.2.2:8000/v1/quizzes/categories"));
       final returnedList = fetchCategories();
-      List<String> list = ["Arts & Literature", "Film & TV", "Food & Drink", "General Knowledge", "Geography", "History", "Music", "Science", "Society & Culture", "Sport & Leisure"];
+      List<String> list = [
+        "Arts & Literature",
+        "Film & TV",
+        "Food & Drink",
+        "General Knowledge",
+        "Geography",
+        "History",
+        "Music",
+        "Science",
+        "Society & Culture",
+        "Sport & Leisure"
+      ];
       FutureBuilder<List<String>>(
           future: returnedList,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               expect(interceptor.isDone, true);
               expect(response.statusCode, 200);
-              expect(response.body, '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}');
-              expect(snapshot.data!,list);
+              expect(response.body,
+                  '{"categories": ["Arts & Literature","Film & TV","Food & Drink","General Knowledge","Geography","History","Music","Science","Society & Culture","Sport & Leisure"]}');
+              expect(snapshot.data!, list);
 
               tester.pumpWidget(const CategoryPage());
               expect(find.byKey(const Key("CPR")), findsOneWidget);
@@ -64,7 +81,7 @@ void main() {
               expect(catName, findsOneWidget);
             }
             return Column();
-      });
+          });
     });
   });
 }
