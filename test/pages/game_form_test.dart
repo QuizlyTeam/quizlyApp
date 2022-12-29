f1a84c5fcc67ca94b70bcdd08d8fbc4403053f5f
Tosiek <antonizieciak@gmail.com>
2022-12-29 15:23:53 +0100

    Wrote some test for game_form.dart

--- test/pages/game_form_test.dart
+++ test/pages/game_form_test.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import 'package:quizly_app/pages/game_form.dart';
 
 void main() {
-  testWidgets('Widget GameForm properly show information',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: GameForm()));
+  testWidgets('Test buttons',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(const MaterialApp(home: GameForm()));
 
-    expect(find.text('Choose your'), findsOneWidget);
-    expect(find.text('game options:'), findsOneWidget);
-    expect(find.text('Private'), findsNWidgets(2));
-    expect(find.text('Public'), findsOneWidget);
-    expect(find.text('Pick'), findsOneWidget);
-    expect(find.text('Play!'), findsOneWidget);
-  });
+        expect(find.text('Play!'), findsOneWidget);
+        expect(find.text('Difficulty level:'), findsOneWidget);
+        expect(find.text('Pick'), findsNWidgets(2));
+        expect(find.byType(ElevatedButton), findsNWidgets(3));
+      });
+
+  testWidgets('Test toggle tabs',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(const MaterialApp(home: GameForm()));
+
+        expect(find.text('Public'), findsOneWidget);
+        expect(find.text('Private'), findsOneWidget);
+        expect(find.text('Easy'), findsOneWidget);
+        expect(find.text('Medium'), findsOneWidget);
+        expect(find.byType(FlutterToggleTab), findsNWidgets(2));
+      });
 }
