b7f09dc5d2552524ee042d0a60c1997ae2d69f0b
Tosiek <antonizieciak@gmail.com>
2023-01-06 22:19:05 +0100

    You can't go to previous screen during game, nor in score screen. But you can go to the menu from score screen.

--- lib/pages/question.dart
+++ lib/pages/question.dart
@@ -269,23 +269,29 @@ class _QuestionState extends State<Question> {
   Widget build(BuildContext context) {
     double x = MediaQuery.of(context).size.width / 411.42857142857144;
     double y = MediaQuery.of(context).size.height / 866.2857142857143;
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SafeArea(
-          child: Scaffold(
-            backgroundColor: Colors.grey[300],
-            appBar: PreferredSize(
-              preferredSize: Size.fromHeight(70 * y),
-              child: Header(
-                leftIcon: 'assets/images/back.png',
-                rightIcon: 'assets/images/settings.png',
-                y: y,
+    return WillPopScope(
+        onWillPop: () async{
+          return false;
+        },
+        child:  MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SafeArea(
+              child: Scaffold(
+                backgroundColor: Colors.grey[300],
+                appBar: PreferredSize(
+                  preferredSize: Size.fromHeight(70 * y),
+                  child: Header(
+                    leftIcon: 'assets/images/back.png',
+                    rightIcon: 'assets/images/settings.png',
+                    y: y,
+                  ),
+                ),
+                body: SingleChildScrollView(
+                  child: bodyOfQuestion(x, y),
+                ),
               ),
-            ),
-            body: SingleChildScrollView(
-              child: bodyOfQuestion(x, y),
-            ),
-          ),
-        ));
+            )
+        )
+    );
   }
 }
--- lib/pages/score.dart
+++ lib/pages/score.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:quizly_app/widgets/header.dart';
+import 'package:quizly_app/pages/menu.dart';
 
 class Score extends StatelessWidget {
   final Map score;
   final String player;
   const Score({super.key, required this.score, required this.player});
 
-  List<Widget> generateTable(double y){
+  List<Widget> generateTable(double y, double x){
     List<Widget> ans = [];
 
     Map sortedScore;
@@ -26,33 +28,57 @@ class Score extends StatelessWidget {
       ans.add(record);
     });
 
+    ElevatedButton returnToMenu = ElevatedButton(
+        onPressed: () {
+          Get.to(const MenuPage());
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.cyan,
+            fixedSize: Size(280 * x, 96 * y),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            )),
+        child: Text(
+          'Return to menu',
+          style: TextStyle(fontSize: 30 * y, color: Colors.white),
+        )
+    );
+
+    ans.add(returnToMenu);
+
     return ans;
   }
 
   @override
   Widget build(BuildContext context) {
     double y = MediaQuery.of(context).size.height / 866.2857142857143;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Scaffold(
-            backgroundColor: Colors.grey[300],
-            appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(70),
-              child: Header(
-                leftIcon: 'assets/images/profile.png',
-                rightIcon: 'assets/images/settings.png',
-                y: y,
-              ),
-            ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: generateTable(y),
-              ),
-            ),
-          )
-      ),
-    );
+    double x = MediaQuery.of(context).size.width / 411.42857142857144;
+
+    return WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child:MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: SafeArea(
+              child: Scaffold(
+                backgroundColor: Colors.grey[300],
+                appBar: PreferredSize(
+                  preferredSize: const Size.fromHeight(70),
+                  child: Header(
+                    leftIcon: 'assets/images/profile.png',
+                    rightIcon: 'assets/images/settings.png',
+                    y: y,
+                  ),
+                ),
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: generateTable(y, x),
+                  ),
+                ),
+              )
+          ),
+        ));
   }
 }
