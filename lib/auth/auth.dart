38dfd540855e1eeb7a92006647ce9a79c9dd6850
KacperST <kstankiewicz@student.agh.edu.pl>
2023-01-07 16:51:52 +0100

    changed

---
+++ lib/auth/auth.dart
@@ -0,0 +1,117 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+import 'package:quizly_app/pages/login_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //sign in anon
+  signInAnon() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print("Signed in with temporary account.");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+    }
+  }
+
+  signOutUser() async {
+    await _auth.signOut();
+    Get.to(() => const LoginPage());
+  }
+
+  signInWithGoogle() async {
+    // Trigger the authentication flow
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+    catch(e){
+      return null;
+    }
+  }
+
+  registerUser(String email, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  loginUser(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      return credential;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+    return null;
+  }
+   signInWithApple() async {
+     try{
+    final appleProvider = AppleAuthProvider();
+    if (kIsWeb) {
+      await FirebaseAuth.instance.signInWithPopup(appleProvider);
+    } else {
+      await FirebaseAuth.instance.signInWithProvider(appleProvider);
+    }
+    }catch(e) {
+      return null;
+    }
+  }
+  signInWithFacebook() async {
+    try {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential = FacebookAuthProvider
+          .credential(loginResult.accessToken!.token);
+
+      // Once signed in, return the UserCredential
+      return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    }catch(e){
+      return null;
+    }
+  }
+}
---
+++ lib/auth/register.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:quizly_app/auth/auth.dart';
+import 'package:get/get.dart';
+import 'package:quizly_app/pages/menu.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    double x = MediaQuery.of(context).size.width / 411.42857142857144;
+    double y = MediaQuery.of(context).size.height / 866.2857142857143;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Register"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20 * x, horizontal: 50 * y),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20 * y,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 3, color: Colors.grey.shade500))),
+                    validator: (value) =>
+                        RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value!)
+                            ? null
+                            : 'Enter a valid email',
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20 * y,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 3, color: Colors.grey.shade500))),
+                    validator: (value) => RegExp(
+                                r'^(?=.*[A-Z])(?=.*[a-z])(?=.*?[0-9]).{6,}$')
+                            .hasMatch(value!)
+                        ? null
+                        : 'Enter a  strong password (1+ upper, 1+ lower, 1+ digit)',
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 20 * y,
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.cyan),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          var result =
+                              await _auth.registerUser(email, password);
+                          if (result != null) {
+                            Get.to(() => const MenuPage());
+                          }
+                        }
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.white),
+                      ))
+                ],
+              ))),
+    );
+  }
+}
