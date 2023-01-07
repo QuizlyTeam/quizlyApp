9da39870502e9ef30f749792ab3bdd1df847811c
Glonk223 <janek.stop@gmail.com>
2023-01-07 18:24:53 +0100

    Blur made darker

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -18,101 +18,105 @@ class SettingsPage extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
         key: const Key("Blur filter"),
-        child: Center(
-            child: Container(
-                width: 317 * x,
-                height: 420 * y,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFE2E2E2),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(45),
-                    topRight: Radius.circular(45),
-                    bottomLeft: Radius.circular(45),
-                    bottomRight: Radius.circular(45),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 77 * y,
-                      width: 317 * x,
-                      decoration: const BoxDecoration(
-                        color: Colors.cyan,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          topRight: Radius.circular(45),
-                          bottomLeft: Radius.circular(45),
-                          bottomRight: Radius.circular(45),
-                        ),
-                      ),
-                      child: Center(
-                          child: DefaultTextStyle(
-                        style: TextStyle(
-                            fontSize: 36 * y,
-                            color: Colors.white,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal),
-                        textAlign: TextAlign.center,
-                        child: const Text(
-                          'Settings',
-                          textDirection: TextDirection.ltr,
-                        ),
-                      )),
+        child: Container(
+          color: Colors.black.withOpacity(0.5),
+          child: Center(
+              child: Container(
+                  width: 317 * x,
+                  height: 420 * y,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE2E2E2),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(45),
+                      topRight: Radius.circular(45),
+                      bottomLeft: Radius.circular(45),
+                      bottomRight: Radius.circular(45),
                     ),
-                    Column(
-                      children: [
-                        SizedBox(
-                          height: 15 * y,
-                        ),
-                        DefaultTextStyle(
-                          style: TextStyle(
-                              fontSize: 36 * y,
-                              color: Colors.black,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal),
-                          textAlign: TextAlign.center,
-                          child: const Text(
-                            'Music',
-                            textDirection: TextDirection.ltr,
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 77 * y,
+                        width: 317 * x,
+                        decoration: const BoxDecoration(
+                          color: Colors.cyan,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(45),
+                            topRight: Radius.circular(45),
+                            bottomLeft: Radius.circular(45),
+                            bottomRight: Radius.circular(45),
                           ),
                         ),
-                        const QuizlySlider(),
-                        DefaultTextStyle(
-                          style: TextStyle(
-                              fontSize: 36 * y,
-                              color: Colors.black,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal),
-                          textAlign: TextAlign.center,
-                          child: const Text(
-                            'Sound',
-                            textDirection: TextDirection.ltr,
-                          ),
-                        ),
-                        const QuizlySlider(),
-                        SizedBox(height: 25 * y),
-                        Center(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.back();
-                            },
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.cyan,
-                                fixedSize: Size(190 * x, 68 * y),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                )),
-                            child: Text(
-                              'Back',
+                        child: Center(
+                            child: DefaultTextStyle(
                               style: TextStyle(
-                                  fontSize: 36 * y, color: Colors.white),
+                                  fontSize: 36 * y,
+                                  color: Colors.white,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.normal),
+                              textAlign: TextAlign.center,
+                              child: const Text(
+                                'Settings',
+                                textDirection: TextDirection.ltr,
+                              ),
+                            )),
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 15 * y,
+                          ),
+                          DefaultTextStyle(
+                            style: TextStyle(
+                                fontSize: 36 * y,
+                                color: Colors.black,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.normal),
+                            textAlign: TextAlign.center,
+                            child: const Text(
+                              'Music',
+                              textDirection: TextDirection.ltr,
                             ),
                           ),
-                        )
-                      ],
-                    )
-                  ],
-                ))),
+                          const QuizlySlider(),
+                          DefaultTextStyle(
+                            style: TextStyle(
+                                fontSize: 36 * y,
+                                color: Colors.black,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.normal),
+                            textAlign: TextAlign.center,
+                            child: const Text(
+                              'Sound',
+                              textDirection: TextDirection.ltr,
+                            ),
+                          ),
+                          const QuizlySlider(),
+                          SizedBox(height: 25 * y),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Get.back();
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.cyan,
+                                  fixedSize: Size(190 * x, 68 * y),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  )),
+                              child: Text(
+                                'Back',
+                                style: TextStyle(
+                                    fontSize: 36 * y, color: Colors.white),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ))),
+        ),
+
       ),
 
       //)
