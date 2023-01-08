60d0b3b8dbb4aeddea0a4f478dbb963775805c3c
KacperST <kstankiewicz@student.agh.edu.pl>
2023-01-08 15:40:04 +0100

    added files

---
+++ lib/classes/user.dart
@@ -0,0 +1,19 @@
+
+class UserToApi{
+  late String uid;
+  final String nickname;
+  late String picture;
+  late int win;
+  late int lose;
+  // ignore: non_constant_identifier_names
+  late String favourite_categoryy;
+  // ignore: non_constant_identifier_names
+  late String max_points;
+   UserToApi({required this.nickname});
+   factory UserToApi.fromJson(Map<String,dynamic> json) {
+      return UserToApi(
+       nickname: json['nickname'],
+     );
+  }
+
+}
