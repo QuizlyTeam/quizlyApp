6101c051f08820f06a0b9ab4d9255a79ce971751
KacperST <kstankiewicz@student.agh.edu.pl>
2023-01-14 13:44:32 +0100

    .

--- lib/pages/game_form.dart
+++ lib/pages/game_form.dart
@@ -506,6 +506,7 @@ class _GameFormState extends State<GameForm> {
         if (snapshot.hasData) {
           quiz = snapshot.data!;
         }
+
         return SizedBox(
             width: 395 * x,
             height: 100 * y,
