281f397effa00e911c53c1a2494886e38e67672c
Tosiek <antonizieciak@gmail.com>
2023-01-15 13:41:48 +0100

    Documented user_account.dart

--- lib/pages/user_account.dart
+++ lib/pages/user_account.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:quizly_app/widgets/header.dart';
 
+/// Show account stats.
+///
+/// This option is only available for logged users. It shows ration between
+/// wins and failures in multiplayer games, favourite category of user and
+/// his highest score.
 // ignore: must_be_immutable
 class UserAccount extends StatelessWidget {
   UserAccount({Key? key, required this.values, this.x = 1, this.y = 1})
@@ -18,33 +23,50 @@ class UserAccount extends StatelessWidget {
         backgroundImage: avatarPicture);
   }
 
+  /// Contains information about wined and lost games.
   late final Map values;
 
+  /// Application background color.
   final Color? _backgroundColor = Colors.grey[300];
+  /// Application color of buttons.
   final Color _appColor = Colors.cyan;
+  /// Scaling factor of horizontal axis of app.
   late double x;
+  /// Scaling factor of vertical axis of app.
   late double y;
 
+  /// Ratio wins:loses.
   late double _progress;
+  ///Users avatar picture.
   late NetworkImage avatarPicture;
 
+  /// Style of text which shows user's nick.
   late final TextStyle nick = TextStyle(
       fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25 * y);
 
+  /// Style of text which shows stat value.
   late final TextStyle regular = TextStyle(
       fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25 * y);
 
+  /// Style of text which shows stat's name.
   late final TextStyle title = TextStyle(
       fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25 * y);
 
+  /// Title of stat 'ratio'.
   late Text ratio = Text('Ratio: ', style: title);
+  /// Value of stat 'ratio'.
   late Text ratioStat =
       Text('${values["win"]}:${values["lose"]}', style: regular);
+  /// Title of stat 'Favourite quiz'.
   late Text favQuiz = Text('Favourite quiz: ', style: title);
+  /// Value of stat 'Favourite quiz'.
   late Text favQuizStat = Text(values["favourite_category"], style: regular);
+  /// Title of stat 'Most points'.
   late Text mostPoints = Text('Most points: ', style: title);
+  /// Value of stat 'Most points'.
   late Text mostPointsStat = Text('${values["max_points"]}', style: regular);
 
+  /// Shows user's nickname.
   late ClipRRect nickname = ClipRRect(
     borderRadius: BorderRadius.circular(30.0),
     child: Container(
@@ -58,8 +80,10 @@ class UserAccount extends StatelessWidget {
     ),
   );
 
+  /// Show's user profile picture.
   late CircleAvatar avatar;
 
+  /// Show's ration win/lose as progress bar.
   late LinearProgressIndicator winsToLoses = LinearProgressIndicator(
     backgroundColor: Colors.red,
     color: Colors.green,
@@ -67,6 +91,7 @@ class UserAccount extends StatelessWidget {
     minHeight: 25 * y,
   );
 
+  /// Nice wraps ratio win/lose with information what is at left and right.
   late Row winsRatio = Row(
     children: [
       Text(
@@ -83,6 +108,7 @@ class UserAccount extends StatelessWidget {
     ],
   );
 
+  /// Wraps wins ratio into fixed size container.
   late Container ratioWrapper = Container(
     alignment: Alignment.center,
     constraints: BoxConstraints(
@@ -92,6 +118,7 @@ class UserAccount extends StatelessWidget {
     child: winsRatio,
   );
 
+  /// Title row of stats' table.
   Container titleField(Text t) {
     return Container(
       alignment: Alignment.center,
@@ -104,6 +131,7 @@ class UserAccount extends StatelessWidget {
     );
   }
 
+  /// Value row of stats' table.
   Container valueField(Text t) {
     return Container(
       alignment: Alignment.center,
@@ -116,6 +144,7 @@ class UserAccount extends StatelessWidget {
     );
   }
 
+  /// Stats' table.
   late Column table = Column(
     children: [
       titleField(ratio),
@@ -127,11 +156,14 @@ class UserAccount extends StatelessWidget {
     ],
   );
 
+  /// Fancy look of stats' table.
   late ClipRRect roundedTable =
       ClipRRect(borderRadius: BorderRadius.circular(30.0), child: table);
 
+  /// Widgets separator.
   late SizedBox someSpace = SizedBox(width: 300 * x, height: 20 * y);
 
+  /// Body of page.
   late Column appBody = Column(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
